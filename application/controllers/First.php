11725dddd835bbc09780f98af342db1066da08c3
Developer <mailendku@gmail.com>
2018-09-13 23:30:01 +0530

    first commit

---
+++ application/controllers/First.php
@@ -0,0 +1,12 @@
+<?php 
+   class First extends CI_Controller {  
+	
+      public function indexx() { 
+         echo "This is default function."; 
+      } 
+  
+      public function hello() { 
+         echo "This is hello function."; 
+      } 
+   } 
+?>
